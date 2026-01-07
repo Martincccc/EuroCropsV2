eeb27f55a0f43ebf8bd1cabaca63e25c2927b635
Martin Claverie <martin.claverie@ec.europa.eu>
2026-01-07 17:31:01 +0100

    relative path

--- code/import_db/update_mapping_tables.py
+++ code/import_db/update_mapping_tables.py
@@ -2,11 +2,14 @@ import pandas as pd
 from utils.tools import to_sql_with_indexes
 
 
-df = pd.read_csv('./data/cropcodemapping/eurocrops.csv')
+df = pd.read_csv('../data/cropcodemapping/eurocrops.csv')
 to_sql_with_indexes(df,'cropmapping.eurocrops',index_cols='all')
 
-df = pd.read_csv('./data/cropcodemapping/hcat4_agriprod_mapping.csv')
+df = pd.read_csv('../data/cropcodemapping/hcat4_agriprod_mapping.csv')
 to_sql_with_indexes(df,'cropmapping.hcat4_agriprod_mapping',index_cols='all')
 
-df = pd.read_csv('./data/cropcodemapping/hcat4_eagle_mapping.csv')
+df = pd.read_csv('../data/cropcodemapping/hcat4_eagle_mapping.csv')
 to_sql_with_indexes(df,'cropmapping.hcat4_eagle_mapping',index_cols='all')
+
+df = pd.read_csv('../data/cropcodemapping/hcat4_hrl_mapping.csv')
+to_sql_with_indexes(df,'cropmapping.hcat4_hrl_mapping',index_cols='all')
